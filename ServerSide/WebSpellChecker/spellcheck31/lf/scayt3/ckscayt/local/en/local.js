7cdd5c084b61783c85e7c6339207b3772d397dfa
Richard Castro Mamani <rcastro@hinantin.com>
2015-04-03 21:30:56 -0500

    Adding part of the server side application

---
+++ ServerSide/WebSpellChecker/spellcheck31/lf/scayt3/ckscayt/local/en/local.js
@@ -0,0 +1,32 @@
+/**
+ * SCAYT v3.0.6950
+ * Copyright (c) 1999-2015 SpellChecker.net, Inc. All rights reserved.
+ */
+
+/*!
+ * jQuip JavaScript Library v0.0.3
+ * https://github.com/mythz/jquip
+ *
+ * Copyright, Demis Bellot
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+/*!
+ * Rangy, a cross-browser JavaScript range and selection library
+ * http://code.google.com/p/rangy/
+ *
+ * Copyright 2012, Tim Down
+ * Licensed under the MIT license.
+ * Version: 1.3alpha.681
+ * Build date: 20 July 2012
+ */
+
+/*! @license
+ * JS Signals <http://millermedeiros.github.com/js-signals/>
+ * Released under the MIT license
+ * Author: Miller Medeiros
+ * Version: 0.8.1 - Build: 266 (2012/07/31 03:33 PM)
+ */ 
+!function(a,b){a&&(a.prototype.Localization.lang[b]={btn_about:"About SCAYT",btn_addWord:"Add Word",btn_createDic:"Create",btn_deleteDic:"Delete",btn_disable:"Disable SCAYT",btn_enable:"Enable SCAYT",btn_ignore:"Ignore",btn_ignoreAll:"Ignore All",btn_langs:"Languages",btn_moreSuggestions:"More suggestions",btn_noSuggestions:"No suggestion",btn_renameDic:"Rename",btn_restoreDic:"Restore",btn_toggle:"Toggle SCAYT",btn_options:"Options",label_allCaps:"Ignore All-Caps Words",label_fieldNameDic:"Dictionary name",label_ignoreDomainNames:"Ignore Domain Names",label_mixedCase:"Ignore Words with Mixed Case",label_mixedWithDigits:"Ignore Words with Numbers",message_info_emptyDic:"Dictionary name should not be empty.",message_info_opera:"Not supported by Opera",tab_about:"About",tab_dictionaries:"Dictionaries",tab_languages:"Languages",tab_options:"Options",text_descriptionDic:"Initially the User Dictionary is stored in a Cookie. However, Cookies are limited in size. When the User Dictionary grows to a point where it cannot be stored in a Cookie, then the dictionary may be stored on our server. To store your personal dictionary on our server you should specify a name for your dictionary. If you already have a stored dictionary, please type its name and click the Restore button.",text_title:"Spell Check As You Type",text_copyrights:"&copy; <a style='text-decoration: underline;cursor:pointer;color:#00f' href='http://www.webspellchecker.net/' target='_blank'>1999-2015 SpellChecker.net, Inc.</a><br />All Rights Reserved.",version:"Version: ",message_success_createDic:"Dictionary %s has been successfully created.",message_error_createDic:"Dictionary %s cannot be created.",message_success_removeDic:"Dictionary %s has been successfully removed.",message_error_removeDic:"Dictionary %s cannot be removed.",message_success_restoreDic:"Dictionary %s has been successfully restored.",message_error_restoreDic:"Dictionary %s cannot be restored.",message_success_renameDic:"Dictionary %s has been successfully renamed.",message_error_renameDic:"Dictionary %s cannot be renamed."})}(window.SCAYT,"en");
---
+++ ServerSide/WebSpellChecker/spellcheck31/lf/scayt3/ckscayt/local/es/local.js
@@ -0,0 +1,32 @@
+/**
+ * SCAYT v3.0.6950
+ * Copyright (c) 1999-2015 SpellChecker.net, Inc. All rights reserved.
+ */
+
+/*!
+ * jQuip JavaScript Library v0.0.3
+ * https://github.com/mythz/jquip
+ *
+ * Copyright, Demis Bellot
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+/*!
+ * Rangy, a cross-browser JavaScript range and selection library
+ * http://code.google.com/p/rangy/
+ *
+ * Copyright 2012, Tim Down
+ * Licensed under the MIT license.
+ * Version: 1.3alpha.681
+ * Build date: 20 July 2012
+ */
+
+/*! @license
+ * JS Signals <http://millermedeiros.github.com/js-signals/>
+ * Released under the MIT license
+ * Author: Miller Medeiros
+ * Version: 0.8.1 - Build: 266 (2012/07/31 03:33 PM)
+ */ 
+!function(a,b){a&&(a.prototype.Localization.lang[b]={btn_about:"Acerca de Corrector",btn_addWord:"Añadir palabra",btn_createDic:"Crear",btn_deleteDic:"Borrar",btn_disable:"Desactivar Corrector",btn_enable:"Activar Corrector",btn_ignore:"Ignorar",btn_ignoreAll:"Ignorar Todas",btn_langs:"Idiomas",btn_moreSuggestions:"Más sugerencias",btn_noSuggestions:"No suggestion",btn_renameDic:"Renombrar",btn_restoreDic:"Recuperar",btn_toggle:"Cambiar Corrector",btn_options:"Opciones",label_allCaps:"Omitir palabras en MAYÚSCULAS",label_fieldNameDic:"Nombre del diccionario",label_ignoreDomainNames:"Omitir nombres de dominio",label_mixedCase:"Ignorar palabras con combinación de mayúsculas y minúsculas",label_mixedWithDigits:"Omitir palabras con números",message_info_emptyDic:"El nombre del diccionario no puede estar en blanco.",message_info_opera:"No soportado en Opera",tab_about:"Acerca de",tab_dictionaries:"Diccionarios",tab_languages:"Idiomas",tab_options:"Opciones",text_descriptionDic:"Inicialmente el Diccionario de usuario se guarda en una Cookie. Sin embargo, las cookies están limitadas en tamaño. Cuando el diccionario crece a un punto en el que no se puede guardar en una Cookie, el diccionario puede ser almacenado en nuestro servidor. Para almacenar su diccionario personalizado en nuestro servidor debe especificar un nombre para su diccionario. Si ya ha guardado un diccionaro, por favor, escriba su nombre y pulse el botón Recuperar",text_title:"Comprobar Ortografía Mientras Escribe",text_copyrights:"&copy; <a style='text-decoration: underline;cursor:pointer;color:#00f' href='http://www.webspellchecker.net/' target='_blank'>1999-2015 SpellChecker.net, Inc.</a><br />All Rights Reserved.",version:"Version: ",message_success_createDic:"Dictionary %s has been successfully created.",message_error_createDic:"Dictionary %s cannot be created.",message_success_removeDic:"Dictionary %s has been successfully removed.",message_error_removeDic:"Dictionary %s cannot be removed.",message_success_restoreDic:"Dictionary %s has been successfully restored.",message_error_restoreDic:"Dictionary %s cannot be restored.",message_success_renameDic:"Dictionary %s has been successfully renamed.",message_error_renameDic:"Dictionary %s cannot be renamed."})}(window.SCAYT,"es");
