1c73f55b55b493794b9215dd27ed4881449ba009
Richard Castro <rcastro@hinantin.com>
2015-04-19 15:47:09 -0500

    Adding changes

--- ServerSide/Hinantin/WebApp/index.php
+++ ServerSide/Hinantin/WebApp/index.php
@@ -1,9 +1,16 @@
 <?php
 include("BaseXClient.php");
 try {
-  $session = new Session("localhost", 1984, "admin", "admin");
-  $session->execute("OPEN HNTErrorCorpus");
-  $input = "for \$id in subsequence(db:open('HNTErrorCorpus')//document/@id, 1, 10) return <doc>{data(\$id)}</doc>";
+  $ini_array = parse_ini_file("/usr/lib/cgi-bin/spellcheck31/script/ConfigFile.ini", true);
+  $HOST = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["HOST"];
+  $USER = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["USER"];
+  $PASSWORD = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PASSWORD"];
+  $DATABASE = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["DATABASE"];
+  $PORT = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PORT"];
+  
+  $session = new Session($HOST, $PORT, $USER, $PASSWORD);
+  $session->execute("OPEN $DATABASE");
+  $input = "for \$id in subsequence(db:open('$DATABASE')//document/@id, 1, 10) return <doc>{data(\$id)}</doc>";
   $query = $session->query($input);
     // loop through all results
     while($query->more()) {
