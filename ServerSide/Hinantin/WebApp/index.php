71606013e34c2555ef8ff9962fafb7d5e6656d8f
Richard Castro <rcastro@hinantin.com>
2015-04-19 17:53:06 -0500

    Adding the dojo grid for listing the files within the error corpus

--- ServerSide/Hinantin/WebApp/index.php
+++ ServerSide/Hinantin/WebApp/index.php
@@ -5,49 +5,77 @@ include("BaseXClient.php");
 <html>
 <head>
 <title>Allin Qillqay - Hinantin Management System</title>
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/dojo/1.9.1/dojo/resources/dojo.css"/>
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/dojo/1.9.1/dijit/themes/claro/claro.css"/>
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/dojo/1.9.1/dojox/grid/resources/Grid.css"/>
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/dojo/1.9.1/dojox/grid/resources/claroGrid.css"/>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/dojo/1.9.1/dojo/dojo.js" djConfig="parseOnLoad: true"></script>
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript">
+	dojo.require('dojox.grid.DataGrid');
+	dojo.require('dojo.data.ItemFileWriteStore');
+	dojo.require('dijit.form.Button');
+	dojo.addOnLoad(function() {
+	    var grid = dijit.byId('myDataGrid');
+	    
+	    <?php
+	    		try {
+	    		  $ini_array = parse_ini_file("/usr/lib/cgi-bin/spellcheck31/script/ConfigFile.ini", true);
+	    		  $HOST = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["HOST"];
+	    		  $USER = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["USER"];
+	    		  $PASSWORD = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PASSWORD"];
+	    		  $DATABASE = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["DATABASE"];
+	    		  $PORT = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PORT"];
+	    		  
+	    		  $session = new Session($HOST, $PORT, $USER, $PASSWORD);
+	    		  $session->execute("OPEN $DATABASE");
+	    		  /* This is a sample id */
+	    		  $id = "doc_20150418005816";
+	    		  $input = "for \$id in subsequence(db:open('$DATABASE')//document/@id, 1, 100) return concat(\"{id: '\", data(\$id), \"',name: '\", data(\$id), \".xml'},\")";
+	    		  $query = $session->query($input);
+	    		  // loop through all results
+	    		  print "var itemList = [";
+	    		  while($query->more()) {
+	    		    print $query->next()."\n";
+	    		  }
+	    		  print "];";
+	    		  $session->close();
+	    		} catch (Exception $e) {
+	    		  // print exception
+	    		  print $e->getMessage();
+	    		}
+	    		?>
+	    
+
+	    var store = new dojo.data.ItemFileWriteStore({
+	        data: {
+	            items: itemList
+	        }
+	    });
+	    grid.setStore(store);
+	});
+	</script>
 </head>
-<body>
+<body class="claro">
 <?php
-try {
-  /*
-   * Basic Example
-   * 
-   * */
-  
-  $ini_array = parse_ini_file("/usr/lib/cgi-bin/spellcheck31/script/ConfigFile.ini", true);
-  $HOST = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["HOST"];
-  $USER = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["USER"];
-  $PASSWORD = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PASSWORD"];
-  $DATABASE = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["DATABASE"];
-  $PORT = $ini_array["PRODUCTION_BASEX_HNTErrorCorpus"]["PORT"];
-  
-  $session = new Session($HOST, $PORT, $USER, $PASSWORD);
-  $session->execute("OPEN $DATABASE");
-  /* This is a sample id */
-  $id = "doc_20150418005816";
-  $input = "for \$id in subsequence(db:open('$DATABASE')//document/@id, 1, 10) return <doc>{data(\$id)}</doc>";
-  $query = $session->query($input);
-  // loop through all results
-  while($query->more()) {
-    print $query->next()."\n";
-  }
-  $session->close();
-  $xslDoc = new DOMDocument();
-  $xslDoc->load("collection.xsl");
+$xslDoc = new DOMDocument();
+$xslDoc->load("collection.xsl");
 
-  $xmlDoc = new DOMDocument();
-  $xmlDoc->load("collection.xml");
-  //$doc->loadXML('<root><node/></root>');
+$xmlDoc = new DOMDocument();
+$xmlDoc->load("collection.xml");
+//$doc->loadXML('<root><node/></root>');
 
-  $proc = new XSLTProcessor();
-  $proc->importStylesheet($xslDoc);
-  echo $proc->transformToXML($xmlDoc);
-
-} catch (Exception $e) {
-  // print exception
-  print $e->getMessage();
-}
-
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xslDoc);
+echo $proc->transformToXML($xmlDoc);
 ?>
+    <table id="myDataGrid" dojoType="dojox.grid.DataGrid" style="width:400px;">
+        <thead>
+        <tr>
+        	<th field="id" width="40%">Identifier</th>
+            <th field="name" width="60%">File Name</th>
+        </tr>
+        </thead>
+    </table>
 </body>
 </html>
