bcd673de4d29f2eb2d426cd8d825b79488e69a40
Richard Castro Mamani <rcastro@hinantin.com>
2015-04-19 14:26:57 -0500

    Adding changes

--- ServerSide/Hinantin/WebApp/index.php
+++ ServerSide/Hinantin/WebApp/index.php
@@ -1,13 +1,28 @@
 <?php
+include("BaseXClient.php");
+try {
+  $session = new Session("localhost", 1984, "admin", "admin");
+  $session->execute("OPEN HNTErrorCorpus");
+  $input = "for \$id in subsequence(db:open('HNTErrorCorpus')//document/@id, 1, 10) return <doc>{data(\$id)}</doc>";
+  $query = $session->query($input);
+    // loop through all results
+    while($query->more()) {
+      print $query->next()."\n";
+    }
+  $session->close();
+  $xslDoc = new DOMDocument();
+  $xslDoc->load("collection.xsl");
 
-   $xslDoc = new DOMDocument();
-   $xslDoc->load("collection.xsl");
+  $xmlDoc = new DOMDocument();
+  $xmlDoc->load("collection.xml");
 
-   $xmlDoc = new DOMDocument();
-   $xmlDoc->load("collection.xml");
+  $proc = new XSLTProcessor();
+  $proc->importStylesheet($xslDoc);
+  echo $proc->transformToXML($xmlDoc);
 
-   $proc = new XSLTProcessor();
-   $proc->importStylesheet($xslDoc);
-   echo $proc->transformToXML($xmlDoc);
+} catch (Exception $e) {
+  // print exception
+  print $e->getMessage();
+}
 
 ?>
