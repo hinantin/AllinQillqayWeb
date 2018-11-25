b664efe9deb0457f98c9e439880cb3d55140234b
rcastromamani <richard.castro.mamani@gmail.com>
2018-11-24 19:06:40 -0500

    Removing fork process from script (they were causing problems on the server.)

--- ServerSide/SQUOIA/foma/fomaserver.c
+++ ServerSide/SQUOIA/foma/fomaserver.c
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
         //}
         //if (pid == 0) {
             /* This is the client process */
-            close(listenfd);
+            //close(listenfd);
             do {
                 byte_count = recv(connfd, sendBuff, 16384, 0);
                 if (byte_count < 0) {
